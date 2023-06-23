fc7a362329cab906c70be195f407328f0b29783f
Renzonr8 <40187955@roctilburg.nl>
2023-06-23 10:17:57 +0200

    laatste push met kleine aanpassingen

--- src/App.js
+++ src/App.js
@@ -11,56 +11,72 @@ function App() {
       url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/0/203817_nike-phantom-gx-elite-dynamic-fit-gras-voetbalschoenen-fg-wit-felblauw-felroze.jpg',
       name: 'Nike phantom GX elite wit/blauw',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
       price: 280
     },
     {
       url: 'https://www.voetbalshop.be/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/216275_nike-phantom-gx-elite-df-gras-voetbalschoenen-fg-wit-felgeel-zwart.jpg',
       name: 'Nike phantom GX wit/geel',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
       price: 280
     },
     {
-      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/217106_nike-zoom-mercurial-vapor-15-elite-ijzeren-nop-voetbalschoenen-sg-pro-player-fe_1.jpg',
-      name: 'Nike mercurial vapor',
-      category: 'Soft-ground / ijzeren nop',
-      seller: 'Voetbalschoenenstore',
+      url: 'https://www.knvbshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/0/203953_nike-zoom-mercurial-superfly-9-elite-gras-voetbalschoenen-fg-wit-felblauw-felro.jpg',
+      name: 'Nike mercurial superfly',
+      category: 'Gras/kunstgras',
       price: 320
     },
     {
       url: 'https://www.voetbalshop.nl/media/catalog/product/cache/3a8fffc51952d94f7bcbf926108ab2b0/1/9/191170_nike-zoom-mercurial-superfly-9-elite-cr7-gras-voetbalschoenen-fg-wit-blauw-bron_1.jpg',
       name: 'Nike mercurial superfly elite CR7',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
       price: 240
     },
     {
-      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/218449_adidas-x-speedportal-gras-voetbalschoenen-fg-oranje-zwart-goud.jpg',
-      name: 'Adidas x speedportal oranje/zwart',
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/217410_nike-zoom-mercurial-vapor-15-elite-xxv-gras-voetbalschoenen-fg-zilver-felgeel-z.jpg',
+      name: 'Nike Mercurial vapor elite',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
       price: 300
     },
     {
-      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/212319_adidas-x-speedportal.1-parley-gras-voetbalschoenen-fg-wit-lichtblauw.jpg',
-      name: 'Adidas x speedportal',
+      url: 'https://www.knvbshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/216320_nike-zoom-mercurial-superfly-9-elite-gras-voetbalschoenen-fg-felroze-geel-licht.jpg',
+      name: 'Nike mercurial superfly elite',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
+      price: 269
+    },
+    {
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/1/9/199797_nike-phantom-gt2-elite-ijzeren-nop-voetbalschoenen-sg-anti-clog-zwart-brons-wit.jpg',
+      name: 'Nike phantom gt2 ijzeren noppen',
+      category: 'Gras',
+      price: 200
+    },
+    {
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/0/208205_puma-future-ultimate-ijzeren-nop-voetbalschoenen-sg-zwart-wit.jpg',
+      name: 'Puma Future ultimate IJzeren noppen',
+      category: 'Gras',
       price: 244
     },
     {
-      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/212319_adidas-x-speedportal.1-parley-gras-voetbalschoenen-fg-wit-lichtblauw.jpg',
-      name: 'Adidas x speedportal',
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/210312_puma-future-ultimate-elements-gras-kunstgras-voetbalschoenen-mg-zwart-oranje-mu.jpg',
+      name: 'Puma Future ultimate Limited edition',
+      category: 'Gras/ kunstgras',
+      price: 450
+    },
+    {
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/218287_adidas-predator-accuracy-veterloze-gras-voetbalschoenen-fg-oranje-zwart_1.jpg',
+      name: 'Adidad predator accuracy',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
       price: 244
     },
     {
-      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/2/1/212319_adidas-x-speedportal.1-parley-gras-voetbalschoenen-fg-wit-lichtblauw.jpg',
-      name: 'Adidas x speedportal',
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/1/7/173896_adidas-x-speedportal-messi.1-gras-voetbalschoenen-fg-wit-rood-zwart.jpg',
+      name: 'Adidas X speedportal Leo Messi',
       category: 'Gras/kunstgras',
-      seller: 'Voetbalschoenenstore',
+      price: 244
+    },
+    {
+      url: 'https://www.voetbalshop.nl/media/catalog/product/cache/d81c8dc66c69ceb69419c2e7e72e896d/1/6/164167_adidas-x-speedportal.1-gras-voetbalschoenen-fg-zwart-rood-groen_1.jpg',
+      name: 'Adidas X speedportal zwart/rood',
+      category: 'Gras',
       price: 244
     },
   ])
